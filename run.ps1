3d6126e489c0d25879b7cc99a610fc6741d70686
Aditya Singh <adityasingh740745@gmail.com>
2024-11-02 13:11:06 +0530

    fix the docs for automated script file for pwrshell

--- run.ps1
+++ run.ps1
@@ -1,7 +1,9 @@
 # If you get a permission error, you may need to change the PowerShell execution policy.
-# Open PowerShell as Administrator and run one of these commands:
-# Option 1 (Recommended): Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
-# Option 2: Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process
+# Open PowerShell as Administrator and run one of the following commands:
+# Option 1: Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+# Option 2: Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope CurrentUser
+
+# Dont worry about the command, it wont affect your system or privacy.
 # Then run the script with: .\run.ps1
 
 Write-Host ("-" * 100)
